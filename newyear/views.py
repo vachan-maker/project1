f15b90daf23320778ddc2bb3a2208e7257f62993
vachan-maker <vachan@tutanota.com>
2025-05-30 15:28:52 +0530

    Added newyear check and display page if newyear or not

--- newyear/views.py
+++ newyear/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    now = datetime.now()
+    if(now.month == 1 and now.day == 1):
+        return HttpResponse("Happy New Year")
+    else:
+        return HttpResponse("There is still time my friend")
