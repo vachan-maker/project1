31f3688ed2cfda6b26e2c5688048002d1d303425
vachan-maker <vachan@tutanota.com>
2025-05-30 11:40:07 +0530

    include all the urls of hello app

--- project1/urls.py
+++ project1/urls.py
@@ -1,3 +1,4 @@
+from django.urls import include,path
 """
 URL configuration for project1 project.
 
@@ -19,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/',include("hello.urls"))
 ]
