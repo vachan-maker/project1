ddb9b574ae2419a8706c3b99794d3d3d69f190dd
vachan-maker <vachan@tutanota.com>
2025-05-30 11:45:05 +0530

    created a new view

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World!")
+
+def vachan(request):
+    return HttpResponse("Hello,vachan")
