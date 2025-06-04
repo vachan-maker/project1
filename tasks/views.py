93e645907c66485d0167536b2ae49218d5384669
vachan-maker <vachan@tutanota.com>
2025-06-04 17:47:21 +0530

    Added integer field and some client side validation

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django import forms
 tasks = ["foo","bar","baz"]
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="newtask")
+    number = forms.IntegerField(label="Priority",min_value=1,max_value=10)
 # Create your views here.
 
 def index(request):
